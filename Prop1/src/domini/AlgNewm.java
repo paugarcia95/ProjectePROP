78426cc760aad50ab288df68d6e0de1945f7b447
Alr67 <rafael.lucena@est.fib.upc.edu>
2015-04-25 18:13:50 +0200

    Rafa: Correció d'estil

--- Prop1/src/domini/AlgNewm.java
+++ Prop1/src/domini/AlgNewm.java
@@ -10,15 +10,14 @@ import java.util.HashSet;
  *
  */
 public class AlgNewm {
+	
 	public HashSet<HashSet<String>> executa(Graf G, Integer percentatge) {
 		return null;
-		//Prova de canvis 
 		
 	}
 	
 	public HashSet<HashSet<String>> executa_num(Graf G, Integer numComunidades) {
 		return null;
-		// Altre prova de a veure que passa
 		
 	}
 	
