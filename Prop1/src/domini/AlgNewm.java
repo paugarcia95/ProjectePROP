13209ad7465a6cb2dae51fa8a4987acdcde786d5
Alr67 <rafael.lucena@est.fib.upc.edu>
2015-04-25 18:02:06 +0200

    Rafa (0.1.0)

---
+++ Prop1/src/domini/AlgNewm.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package domini;
+
+import java.util.HashSet;
+
+/**
+ * @author Alr
+ *
+ */
+public class AlgNewm {
+	public HashSet<HashSet<String>> executa(Graf G, Integer percentatge) {
+		return null;
+		//Prova de canvis 
+		
+	}
+	
+	public HashSet<HashSet<String>> executa_num(Graf G, Integer numComunidades) {
+		return null;
+		// Altre prova de a veure que passa
+		
+	}
+	
+	public HashSet<HashSet<String>> executa_bet(Graf G, Integer maxbetwennesses) {
+		return null;
+		
+		
+	}
+}
