1428046dc0a219a9a70822354cd21a0a2246c23d
Pau <paug.g.11@hotmail.com>
2015-04-25 18:03:18 +0200

    Pau descomentar while

--- Prop1/src/domini/InputOutput.java
+++ Prop1/src/domini/InputOutput.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
-//Hola
+
 public class InputOutput {
 
 	public void loadGraph(String rute) throws FileNotFoundException, IOException {
