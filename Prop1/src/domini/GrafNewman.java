cdc6c8051edc55d6b45c19e8e41cfeebff125efd
cristina.fontanet <cristina.fontanet@C6S306PC43.fib.upc.es>
2015-04-29 11:46:41 +0200

    Reparticio Pau i Cris

--- Prop1/src/domini/GrafNewman.java
+++ Prop1/src/domini/GrafNewman.java
@@ -4,43 +4,61 @@
 package domini;
 
 import java.util.HashSet;
+import java.util.Queue;
+import java.util.Vector;
 
 /**
- * @author Cristina
+ * @author Cristina & Pau
  *
  */
 public class GrafNewman extends Graf {
-
+	
+	private Queue camiMin(int posiA, int posjA, int posiB, int posjB ){
+		return null;	//PAU
+	}
+	
+	protected Vector< Vector<Integer> > NCM;
+	int maxi;
+	int maxj;
+	int maxNumCM;
+	
+	
 	/**
 	 * 
 	 */
 	public GrafNewman() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub //Cris
 	}
 	
 	//Recalcula el nombre de camins mínims que passen per cada vèrtex, retorna false si hi ha hagut algun error.	
-	public Boolean Calculate_edge_between(){
+	public Boolean Calculate_edge_between(){	//CRIS
+		camiMin(1,2,3,4);
+		//Calcula el cami minim de cada node cap a tots els nodes
+		
+		//un cop trobat cada cami minim, sumar 1 a la pos de NCM
+		//mantenir el vertex per on passen mes camins minims (variables maxi, maxj i maxNumCM)
 		return null;
 	}
 
 	//Fa la inversa dels pesos de les arestes del graf, retorna false si hi ha hagut algun error.	
 	public Boolean Invertir_pesos(){
-		return null;
+		return null; //Cris
 	}
 
 	//Esborra l’aresta per la que passen més camins mínims de tot el graf, retorna false si hi ha hagut algun error.
 	public Boolean esborrar_maxim(){
-		return null;
+		//del Graf "original", eliminem (posar a null, infinit o lu q sigui) del graf de pesos la posicio que indiqui la variable maxNumCM i la posem a 0 
+		return null; //Cris
 	}
 
 	//Retorna el nombre de comunitats en les quals està dividit el graf o 1 en cas d’error.
 	public Integer Num_comunitats(){
-		return null;
+		return null; //Pau
 	} 
 
 	//Retorna el conjunt de comunitats existents.
 	public HashSet<HashSet<String>> comunitats(){
-		return null;
+		return null; //Pau
 	}
 
 	
