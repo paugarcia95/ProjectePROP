ed5309ca29ef6f1b316b7c77718f560e7a052a84
Pau <paug.g.11@hotmail.com>
2015-05-01 17:54:26 +0200

    Pau

--- Prop1/src/domini/EntradaSortidaDadesGraf.java
+++ Prop1/src/domini/EntradaSortidaDadesGraf.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
-//he canviat el nom
+
 public class EntradaSortidaDadesGraf {
 
 	public void loadGraph(String rute) throws FileNotFoundException, IOException {
