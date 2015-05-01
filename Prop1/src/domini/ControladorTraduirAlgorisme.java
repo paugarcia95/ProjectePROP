0fce02f7d8cbfa259dbbe5fa0091e696b7960210
Alr67 <rafael.lucena@est.fib.upc.edu>
2015-05-01 19:32:37 +0200

    Rafa: Progrés ctrld

--- Prop1/src/domini/ControladorTraduirAlgorisme.java
+++ Prop1/src/domini/ControladorTraduirAlgorisme.java
@@ -9,4 +9,18 @@ package domini;
  */
 public class ControladorTraduirAlgorisme {
 
+	private Integer CalcularPesEntreCategories(Categoria c1, Categoria c2, Criteris cri){
+		return null;
+		
+	}
+	
+	private Integer CalcularPesEntreCatPag(Categoria c, Pagina p, Criteris cri) {
+		return null;
+		
+	}
+	
+	private Boolean GrafDadestoGraf (GrafDades graf, Criteris cri) {
+		
+		return null;
+	}
 }
