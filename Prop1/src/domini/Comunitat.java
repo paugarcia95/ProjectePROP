66bdac59a122707542ccad43cfa62a82a37a0545
dani <daniel_dolo@hotmail.com>
2015-05-02 11:23:48 +0200

    Dani: Adaptació Comunitat a les normes de programació

--- Prop1/src/domini/Comunitat.java
+++ Prop1/src/domini/Comunitat.java
@@ -10,47 +10,34 @@ import java.util.TreeMap;
 
 public class Comunitat {
 	
-	private Integer Identificador;
-	private Map<String, Categoria> CategoriesComunitat;
-	
-	public Comunitat(Integer id) {
-		Identificador = id;
-		CategoriesComunitat = new TreeMap<String, Categoria>();
-	}
-	
+	private Map<String, Categoria> categoriesComunitat;
+		
 	public Comunitat() {
-		Identificador = -1; 	//Pau: mmm.. aixo podria donar problemes, podrien haver-hi dues comunitats amb el mateix 
-												//     id, ho hauriem de parlar a classe. he vist que has posat que podria ser un string,
-												//	   a mi tambe em sembla bé
-		CategoriesComunitat = new TreeMap<String, Categoria>();
-	}
-	
-	public Integer getId() {
-		return Identificador;
+		categoriesComunitat = new TreeMap<String, Categoria>();
 	}
 	
 	public Integer getNumeroDeCategories() {
-		return CategoriesComunitat.size();	
+		return categoriesComunitat.size();	
 	}
 	
 	public Categoria getCat(String nom) {
-		return CategoriesComunitat.get(nom);
+		return categoriesComunitat.get(nom);
 	}
 	
 	public void addCat(Categoria c) {
-		CategoriesComunitat.put(c.getNom(), c);
+		categoriesComunitat.put(c.getNom(), c);
 	}
 	
 	public void removeCat(String categoria) {
-		CategoriesComunitat.remove(categoria);
+		categoriesComunitat.remove(categoria);
 	}
 	
 	public Map<String, Categoria> getMapCat() {
-		return CategoriesComunitat;
+		return categoriesComunitat;
 	}
 	
 	public void setMapCat(Map<String, Categoria> mapa) {
-		CategoriesComunitat = mapa;
+		categoriesComunitat = mapa;
 	}
 	
 }
