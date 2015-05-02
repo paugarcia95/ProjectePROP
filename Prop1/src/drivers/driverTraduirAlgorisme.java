3663d8d59e26d2bc7a64e00dfa18a5ac5414934a
Alr67 <rafael.lucena@est.fib.upc.edu>
2015-05-02 20:27:28 +0200

    Rafa: Nou Driver! :D

---
+++ Prop1/src/drivers/driverTraduirAlgorisme.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package drivers;
+
+/**
+ * @author Alr
+ *
+ */
+public class driverTraduirAlgorisme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
