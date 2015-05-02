0f69bf5e60d2895cb1477c417c1b34a889385b28
Alr67 <rafael.lucena@est.fib.upc.edu>
2015-05-02 02:07:27 +0200

    Rafa: progrés clase traduir

--- Prop1/src/domini/ControladorTraduirAlgorisme.java
+++ Prop1/src/domini/ControladorTraduirAlgorisme.java
@@ -22,13 +22,41 @@ public class ControladorTraduirAlgorisme {
 		else if(cri.getSubconjCat().contains(c1) && cri.getSubconjCat().contains(c2)) return solucio = maxvalor; //Criteri de només aquell subconjunt de cats
 		else {
 			
+			////////////**************Criteri de cat-cat i cat-pg********/////////////
 			Integer temp = cri.getRelacionsCat(); // Criteri de cat-cat i cat-pg
 			if(temp == 5) solucio += 5;
 			else if(temp > 5) solucio += (5+(temp-5));
 			else solucio += 5-(5-temp);
+			/////////////*********************************************///////////
 			
+			//////////*************Criteri de coincidencia *********//////////
+			String paraulacercada = new String(cri.getParaulaClau().getParaula()); // Criteri nom cat coincident
+			String ncat = new String(c1.getNom());
+			String ncat2 = new String(c2.getNom());
+			Integer criteri = new Integer(cri.getParaulaClau().getNum());
 			
+			if(criteri == 10) { // Cas de que han de contenir EXACTAMENT la paraula
+				if(ncat.indexOf(paraulacercada) != -1 && ncat2.indexOf(paraulacercada) != -1) {
+					return solucio = maxvalor;
+				}
+				else return solucio = 0;
+			}
+			else if (criteri > 5) { // Cas en q és >5, premiem les coincidencies i penalitzem els errors
+				if(ncat.indexOf(paraulacercada) != -1 && ncat2.indexOf(paraulacercada) != -1) {
+					solucio += paraulacercada.length()*criteri; // premio la coincidencia
+					solucio += solucio/2; // Y favorezco que esté en los 2
+					solucio -= ((ncat.length() - paraulacercada.length())*criteri)+((ncat2.length() - paraulacercada.length())*criteri); // Li resto una mica per la diferència
+				}
+				else { // Si no conté a la paraula clau
+					solucio -= paraulacercada.length()*criteri; // Li resto una mica de valor
+				}
+			}
+			else if ( criteri > 0) {
+				if(ncat.indexOf(paraulacercada) != -1) solucio += paraulacercada.length()*criteri; // premio la coincidencia
+			}
+			/////////////////////***********************************//////////////////
 		}
+		if(solucio < 0) solucio = 0; // Evitem que suigi negatiu
 		return solucio;
 	}
 	/**
