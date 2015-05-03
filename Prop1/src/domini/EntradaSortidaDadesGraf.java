103ce89e6d5235005222a3b1a187e596efba7e29
Pau <paug.g.11@hotmail.com>
2015-05-03 14:30:54 +0200

    Pau: solucionats errors d'entrada/sortida + nova funcio per llegir nomes una entrada (per terminal)

--- Prop1/src/domini/EntradaSortidaDadesGraf.java
+++ Prop1/src/domini/EntradaSortidaDadesGraf.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
@@ -15,9 +16,82 @@ import java.util.StringTokenizer;
  */
 
 public class EntradaSortidaDadesGraf {
+	private String rutaPerDefecte;
 
-	public GrafDades carregarGrafDades(String ruta) throws FileNotFoundException, IOException {
-		GrafDades G = new GrafDades();
+	/**
+	 * Creadora per defecte. La ruta per defecte d'inicialitza a null.
+	 * 
+	 */
+	public EntradaSortidaDadesGraf() {
+		this.rutaPerDefecte = null;
+	}
+
+	/**
+	 * Es crea una nova instància de la classe on la ruta per defecte és la
+	 * indicada
+	 * 
+	 */
+	public EntradaSortidaDadesGraf(String rutaPerDefecte) {
+		this.rutaPerDefecte = rutaPerDefecte;
+	}
+
+	/**
+	 * Retorna la ruta per defecte
+	 * 
+	 * @return rutaPerDefecte
+	 */
+	public String getRutaPerDefecte() {
+		return rutaPerDefecte;
+	}
+
+	/**
+	 * S'estableix com a rutaPerDefecte la indicada
+	 * 
+	 * @param rutaPerDefecte
+	 *            ruta des d'on es carregaran els fitxers a l'iniciar el
+	 *            programa
+	 */
+	public void setRutaPerDefecte(String rutaPerDefecte) {
+		this.rutaPerDefecte = rutaPerDefecte;
+	}
+
+	/**
+	 * Carrega les dades d'un fitxer de text a un graf.
+	 * 
+	 * Si inici==TRUE:
+	 * 
+	 * S'utilitza la rutaPerDefecte de la classe per carregar les dades del
+	 * graf. Si la rutaPerDefecte no està inicialitzada, s'inicialitza
+	 * utilitzant "ruta". En aquest cas, es crea un nou GrafDades (s'ignora G),
+	 * s'omple amb les dades indicades al fitxer i es retorna aquest graf.
+	 * 
+	 * Si inici==FALSE:
+	 * 
+	 * S'afegeixen a G els nodes i/o arestes indicats pel fitxer de text a
+	 * "ruta"
+	 * 
+	 * @param inici
+	 *            Booleà que indica si és la primera vegada que es carreguen
+	 *            fitxers al graf G
+	 * @param G
+	 *            Només es té en compte si inici==FALSE; si és així, s'afegeixen
+	 *            a aquest graf els nous nodes i arestes
+	 * @param ruta
+	 *            Adreça on es troba el fitxer de text
+	 * @return Un GrafDades amb les dades de "ruta"
+	 */
+	public GrafDades carregarGrafDades(Boolean inici, GrafDades G, String ruta) throws FileNotFoundException,
+			IOException {
+
+		if (inici) {
+			G = new GrafDades();
+			if (rutaPerDefecte != null)
+				ruta = rutaPerDefecte;
+			else {
+				rutaPerDefecte = ruta;
+			}
+		}
+
 		BufferedReader b = new BufferedReader(new FileReader(ruta));
 		String s;
 
@@ -29,7 +103,6 @@ public class EntradaSortidaDadesGraf {
 				st.nextToken();
 				String link = st.nextToken();
 				String word2 = st.nextToken();
-				st.nextToken();
 
 				if (link.equals("CsubC"))
 					G.addCC(new Categoria(word2), new Categoria(word1));
@@ -38,7 +111,7 @@ public class EntradaSortidaDadesGraf {
 				else if (link.equals("CP"))
 					G.addCP(new Categoria(word1), new Pagina(word2));
 				else if (link.equals("PC"))
-					G.addPC(new Pagina(word2), new Categoria(word1));
+					G.addPC(new Pagina(word1), new Categoria(word2));
 				else
 					System.out.println("Error al crear el graf: Comprova la sintaxi de l'entrada");
 			}
@@ -47,7 +120,16 @@ public class EntradaSortidaDadesGraf {
 		return G;
 	}
 
-	public void escriureGrafDadesEnFitxer(GrafDades G, String ruta) throws FileNotFoundException, IOException {
+	/**
+	 * Guarda la configuració del graf G a un fitxer de text a "ruta"
+	 * 
+	 * @param G
+	 *            Indica el graf de dades que es vol emmagatzemar
+	 * @param ruta
+	 *            Indica la ruta de directoris i el fitxer on es vol guardar el
+	 *            graf
+	 */
+	public void escriureGrafDadesEnFitxer(GrafDades G, String ruta) {
 		FileWriter fichEscr = null;
 		PrintWriter docE = null;
 
@@ -64,8 +146,8 @@ public class EntradaSortidaDadesGraf {
 
 				Iterator<Pagina> CP = c.getMapCP().values().iterator();
 				Iterator<Pagina> PC = c.getMapPC().values().iterator();
-				Iterator<Categoria> CsubC = c.getMapCSubC().values().iterator();
 				Iterator<Categoria> CsupC = c.getMapCSupC().values().iterator();
+				// Iterator<Categoria> CsubC = c.getMapCSubC().values().iterator();
 
 				while (CP.hasNext()) {
 					Pagina p = CP.next();
@@ -74,18 +156,26 @@ public class EntradaSortidaDadesGraf {
 
 				while (PC.hasNext()) {
 					Pagina p = PC.next();
-					docE.println(c1 + " pag PC " + p.getNom() + " cat");
-				}
-
-				while (CsubC.hasNext()) {
-					Categoria c2 = CsubC.next();
-					docE.println(c2.getNom() + " cat CsubC " + c1 + " cat");
+					docE.println(p.getNom() + " pag PC " + c1 + " cat");
 				}
 
 				while (CsupC.hasNext()) {
 					Categoria c2 = CsupC.next();
 					docE.println(c2.getNom() + " cat CsupC " + c1 + " cat");
 				}
+
+				/*
+				 * Aquest no cal perquè sinó es replicarien les dades, igual que
+				 * només imprimeixo les categories
+				 * 
+				 * while (CsubC.hasNext()) {
+				 * 
+				 * Categoria c2 = CsubC.next(); docE.println(c2.getNom() +
+				 * " cat CsubC " + c1 + " cat");
+				 * 
+				 * }
+				 */
+
 			}
 
 		} catch (Exception e) {
@@ -101,6 +191,17 @@ public class EntradaSortidaDadesGraf {
 		}
 	}
 
+	/**
+	 * Tradueix un fitxer de dades d'un graf a un fitxer que permet al programa
+	 * DOT representar graficament un graf. El programa DOT només funciona
+	 * correctament si el graf té menys de 100 nodes aproximadament.
+	 * 
+	 * @param rutaGraf
+	 *            Ruta de directoris que indica on es troba el fitxer amb les
+	 *            dades del graf.
+	 * @param rutaImatgeTXT
+	 *            La ruta de l'arxiu de text de destí.
+	 */
 	public void traduirGrafDadesAImatge(String rutaGraf, String rutaImatgeTXT) {
 		String s;
 
@@ -134,9 +235,9 @@ public class EntradaSortidaDadesGraf {
 
 					word1 = word1.replaceAll("[^a-zA-Z]", "_");
 					// Aixo fa que elimini tots els caracters que no siguin
-					// lletres
+					// lletres o barra_baixes
 					word2 = word2.replaceAll("[^a-zA-Z]", "_");
-					// Perque sino el programa de dibuixargrafs no funciona
+					// Perque sino el programa de dibuixar grafs no funciona
 
 					if (link.equals("CsubC") || link.equals("CP") || link.equals("PC"))
 						docE.println(word1 + " -> " + word2);
@@ -174,5 +275,44 @@ public class EntradaSortidaDadesGraf {
 				e2.printStackTrace();
 			}
 		}
+	}
+
+	/**
+	 * Llegeix del canal standard d'entrada una aresta (en format: node1
+	 * tipusEnllaç node2) i l'afegeix a G.
+	 * 
+	 * @param G
+	 *            El graf al que es vol afegir una Aresta.
+	 * @param OK
+	 *            Indica si la dada s'ha inserit correctament al graf G
+	 * @return Retorna true si l'usuari no ha indicat el final de l'entrada;
+	 *         false en cas contrari
+	 */
+	public Boolean entrarUnaDada(GrafDades G, Boolean OK) {
+		Scanner in = new Scanner(System.in);
+		OK = true;
+
+		String element1 = in.next();
+
+		if (element1.equals("#")) {
+			OK = false;
+			return false;
+		}
+
+		String link = in.next();
+		String element2 = in.next();
+
+		if (link.equals("CP")) {
+			OK = G.addCP(new Categoria(element1), new Pagina(element2));
+		} else if (link.equals("PC")) {
+			OK = G.addPC(new Pagina(element1), new Categoria(element2));
+		} else if (link.equals("CsubC")) {
+			OK = G.addCC(new Categoria(element1), new Categoria(element2));
+		} else if (link.equals("CsupC")) {
+			OK = G.addCC(new Categoria(element2), new Categoria(element1));
+		} else
+			OK = false;
+
+		return true;
 	}
 }
