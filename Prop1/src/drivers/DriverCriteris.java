545575c9243cb436cc23540feeef3b28d034666a
dani <daniel_dolo@hotmail.com>
2015-05-03 23:59:55 +0200

    Dani: Driver Criteris

---
+++ Prop1/src/drivers/DriverCriteris.java
@@ -0,0 +1,14 @@
+package drivers;
+
+/**
+ * @author Dani
+ *
+ */
+
+public class DriverCriteris {
+	
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(CriterisTest.class);
+	}
+
+}
