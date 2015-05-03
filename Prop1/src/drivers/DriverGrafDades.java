a51e61683e52fbcba653ddeeb7a9f588f811696f
dani <daniel_dolo@hotmail.com>
2015-05-03 16:47:56 +0200

    Dani: Driver Usuari "acabat" i plantilla Driver GrafDades

---
+++ Prop1/src/drivers/DriverGrafDades.java
@@ -0,0 +1,13 @@
+package drivers;
+
+/**
+ * @author Dani
+ *
+ */
+
+public class DriverGrafDades {
+	
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(GrafDadesTest.class);
+	}
+}
---
+++ Prop1/src/drivers/GrafDadesTest.java
@@ -0,0 +1,12 @@
+package drivers;
+
+import domini.GrafDades;
+import junit.framework.TestCase;
+
+public class GrafDadesTest extends TestCase {
+
+	public GrafDadesTest() {
+		super("GrafDadesTest");
+	}
+
+}
